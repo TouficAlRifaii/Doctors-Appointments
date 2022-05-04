0b69b116584ff46cf6c8736f9a531fdf4fee9f00
Toufic Al Rifaii <toufic.alrifaii@lau.edu>
2022-05-04 20:09:33 +0300

    signUp UI

--- Frontend/Doctorsappointments/app/src/main/java/com/example/doctorsappointments/LoginPage.java
+++ Frontend/Doctorsappointments/app/src/main/java/com/example/doctorsappointments/LoginPage.java
@@ -2,7 +2,9 @@ package com.example.doctorsappointments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -11,5 +13,9 @@ public class LoginPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
         getSupportActionBar().hide();
+    }
+    public void toSignUp(View v){
+        Intent intent = new Intent(this, SignUpPage.class);
+        startActivity(intent);
     }
 }
--- Frontend/Doctorsappointments/app/src/main/java/com/example/doctorsappointments/SignUpPage.java
+++ Frontend/Doctorsappointments/app/src/main/java/com/example/doctorsappointments/SignUpPage.java
@@ -2,7 +2,9 @@ package com.example.doctorsappointments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpPage extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SignUpPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+        getSupportActionBar().hide();
+    }
+    public void toLogin(View v){
+        Intent intent = new Intent(this , LoginPage.class);
+        startActivity(intent);
     }
 }
